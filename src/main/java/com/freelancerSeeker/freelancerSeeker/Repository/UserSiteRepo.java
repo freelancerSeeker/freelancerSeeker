5bcc741e75f389f4c1c3083e3fae3d5b549289aa
MarwaHus <marwa.hu18@gmail.com>
2023-10-01 11:45:01 +0300

    profile

--- src/main/java/com/freelancerSeeker/freelancerSeeker/Repository/UserSiteRepo.java
+++ src/main/java/com/freelancerSeeker/freelancerSeeker/Repository/UserSiteRepo.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserSiteRepo extends JpaRepository<UserSite,Long> {
     UserSite findByUsername(String username);
 
+
 }
