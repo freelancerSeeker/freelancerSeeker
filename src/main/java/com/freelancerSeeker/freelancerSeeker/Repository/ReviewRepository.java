6a5a0506f614455002f91c252125912499f84ff1
Abarham97 <abodzbarham@gmailcom>
2023-10-04 11:21:31 +0300

    review

--- src/main/java/com/freelancerSeeker/freelancerSeeker/Repository/ReviewRepository.java
+++ src/main/java/com/freelancerSeeker/freelancerSeeker/Repository/ReviewRepository.java
@@ -4,6 +4,11 @@ package com.freelancerSeeker.freelancerSeeker.Repository;
 import com.freelancerSeeker.freelancerSeeker.Entity.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<ReviewEntity,Long>{
+
+
+
 
 }
